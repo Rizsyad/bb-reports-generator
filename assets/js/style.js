35b91db06128695159ad2fbc77fd36cfa8fbb911
rizsyad <rizsyad@gmail.com>
2023-02-10 07:16:07 +0700

    reporting xss

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,51 +1,47 @@
 $(document).ready(function () {
-  let template = templates;
   $("#send").addClass("disabled");
   $("#send").attr("href", "#");
   $("#other").hide();
 
-  function generateTemplates() {
+  const getDataJSON = async (fileName) => {
+    const fileJSON = `reporting/${fileName}.json`;
+    const response = await fetch(fileJSON);
+    const json = await response.json();
+    return json;
+  };
+
+  const generateTemplates = async () => {
     let name = $("#yn").val();
     let program = $("#pn").val();
     let linkWebsite = $("#lw").val();
     let bugs = $("#bugo").val() || $("#bug option:selected").val();
+    let report = $("#report").val();
     let poc = $("#poc").val();
-    let remediation = $("#remediation").val();
-    let refrences = $("#refrences").val();
-    let impact = $("#impact").val();
-    let severity = $("#severity").val();
+    let remediationC = $("#remediation").val();
+    let refrencesC = $("#refrences").val();
+    let impectC = $("#impect").val();
+    let severityC = $("#severity").val();
+    let language = $("#lang").val();
+    let to = $("#email").val();
 
-    template = template.replace("{{name}}", name);
+    const reporting = await getDataJSON(language);
+    let template = reporting.templates;
+
+    const { description, remediation, refrences, impect, severity, subject } =
+      reporting?.[bugs];
+
+    template = template.replace(new RegExp("{{name}}", "g"), name);
     template = template.replace("{{program}}", program);
     template = template.replace(new RegExp("{{website}}", "g"), linkWebsite);
     template = template.replace(new RegExp("{{bugs}}", "g"), bugs);
     template = template.replace("{{poc}}", poc);
-
-    // information bugs
-    template = template.replace(
-      "{{remediation}}",
-      template_informations?.[bugs]?.remediation || remediation
-    );
-
-    template = template.replace(
-      "{{refrences}}",
-      template_informations?.[bugs]?.refrences || refrences
-    );
-
-    template = template.replace(
-      "{{impact}}",
-      template_informations?.[bugs]?.impact || impact
-    );
-
-    template = template.replace(
-      "{{severity}}",
-      template_informations?.[bugs]?.severity || severity
-    );
-
+    template = template.replace("{{report}}", report);
+    template = template.replace("{{description}}", description);
+    template = template.replace("{{remediation}}", remediation || remediationC);
+    template = template.replace("{{refrences}}", refrences || refrencesC);
+    template = template.replace("{{impect}}", impect || impectC);
+    template = template.replace("{{severity}}", severity || severityC);
     $("#temp").val(template);
-
-    let to = $("#email").val();
-    let subject = `vulnerability ${bugs} on ${linkWebsite}`;
 
     if (to == "") {
       $("#send").addClass("disabled");
@@ -59,7 +55,7 @@ $(document).ready(function () {
         )}&body=${encodeURIComponent(template)}`
       );
     }
-  }
+  };
 
   $("#bug").on("change", function () {
     let value = $(this).val();
@@ -73,6 +69,9 @@ $(document).ready(function () {
 
   $("#generate").on("click", function () {
     generateTemplates();
+  });
+
+  $("#copy").on("click", function () {
     toastr.success("Copy to clipboard");
     navigator.clipboard.writeText($("#temp").val());
   });
